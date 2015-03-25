66a490813c993b2db7ecd5428524073a3f52caf5
Jason Daly <jason@deefour.me>
2015-03-25 16:04:49 -0400

    Move scopeable class resolution into proper contract

--- src/Authorizer/Contracts/Authorizable.php
+++ src/Authorizer/Contracts/Authorizable.php
@@ -10,26 +10,11 @@ interface Authorizable {
   public function policyNamespace();
 
   /**
-   * The namespace to use for the scope class lookups.
-   *
-   * @return string
-   */
-  public function scopeNamespace();
-
-  /**
    * Generates the name of the policy class, usually based off of the name of the
    * class implementing this contract.
    *
    * @return string
    */
   public function policyClass();
-
-  /**
-   * Generates the name of the scope class, usually based off of the name of the
-   * class implementing this contract.
-   *
-   * @return string
-   */
-  public function scopeClass();
 
 }
--- src/Authorizer/Contracts/Scopeable.php
+++ src/Authorizer/Contracts/Scopeable.php
@@ -3,6 +3,21 @@
 interface Scopeable {
 
   /**
+   * The namespace to use for the scope class lookups.
+   *
+   * @return string
+   */
+  public function scopeNamespace();
+
+  /**
+   * Generates the name of the scope class, usually based off of the name of the
+   * class implementing this contract.
+   *
+   * @return string
+   */
+  public function scopeClass();
+
+  /**
    * Common entry-point for the base query object the scope resolution will be
    * based off of.
    *
