7655220fb5b78ef1009b8384871a1856f26958b0
Jason Daly <jason@deefour.me>
2015-05-25 11:06:02 -0400

    docs for helpers. scope helper now supports query builders

--- src/Authorizer/helpers.php
+++ src/Authorizer/helpers.php
@@ -1,13 +1,19 @@
 <?php
-
+use Deefour\Authorizer\Authorizer;
 use Deefour\Authorizer\Contracts\Authorizable;
 use Deefour\Authorizer\Contracts\ResolvesAuthorizable;
 use Deefour\Authorizer\Contracts\Scopeable;
 use Deefour\Authorizer\Exceptions\NotScopeableException;
+use Illuminate\Database\Eloquent\Builder;
 
 if ( ! function_exists('policy')) {
   /**
-   * Assign high numeric IDs to a config item to force appending.
+   * Retrieve policy class for the passed object. The argument can be a FQCN
+   * or an identifier that can be resolved through Laravel's service container.
+   *
+   * If the resolved class itself is not authorizable but implements the
+   * ResolvesAuthorizable contract, resolution will be performed by calling through
+   * to that method.
    *
    * @param  Authorizable|string $object
    *
@@ -30,17 +36,31 @@ if ( ! function_exists('policy')) {
 
 if ( ! function_exists('scope')) {
   /**
-   * Assign high numeric IDs to a config item to force appending.
+   * Retrieve a scoped query for the passed object. The argument can be a FQCN,
+   * an identifier that can be resolved through Laravel's service container, or
+   * an Eloquent query builder instance.
+   *
+   * The resulting object must implement the Scopeable interface. If a query builder
+   * instance is received, the model class will be fetched from it.
+   *
+   * If the original object passed is Scopeable, the 'base scope' will be pulled
+   * from that object as the root of the query.
+   *
+   * A query builder instance will be returned.
    *
    * @param  Scopeable|string $object
    *
-   * @return array
+   * @return Builder
+   * @throws NotScopeableException
    */
   function scope($object) {
+    $scope      = $object;
     $authorizer = app('authorizer');
 
     if (is_string($object)) {
       $object = app($object);
+    } elseif ($object instanceof Builder) {
+      $object = $object->getModel();
     }
 
     if ( ! ($object instanceof Scopeable)) {
@@ -52,15 +72,20 @@ if ( ! function_exists('scope')) {
       ));
     }
 
-    return $authorizer->scope($object);
+    if ($object === $scope) {
+      $scope = $object->getBaseScope();
+    }
+
+    return $authorizer->scope($object, $scope);
   }
 }
 
 if ( ! function_exists('authorizer')) {
   /**
-   * Assign high numeric IDs to a config item to force appending.
+   * Resolve an implementation of the authorization manager from Laravel's service
+   * container.
    *
-   * @return \Deefour\Authorizer\Authorizer
+   * @return Authorizer
    */
   function authorizer() {
     return app('authorizer');
