cf8c211c7305fb1df1a12b38056d4a48d2ca5762
Jason Daly <jason@deefour.me>
2015-08-08 14:09:52 -0400

    passthru policy/scope generation for authorizee

--- src/Contracts/Authorizee.php
+++ src/Contracts/Authorizee.php
@@ -2,7 +2,26 @@
 
 namespace Deefour\Authorizer\Contracts;
 
+use Deefour\Authorizer\Policy;
+use Deefour\Authorizer\Scope;
+
 interface Authorizee
 {
-    //
+    /**
+     * Generate a policy for the authorizable, using the this class instance
+     * as the authorizee.
+     *
+     * @param Authorizable $authorizable
+     * @return Policy
+     */
+    public function policyFor(Authorizable $authorizable);
+
+    /**
+     * Generate a scope for the authorizable, using the this class instance
+     * as the authorizee.
+     *
+     * @param Authorizable $authorizable
+     * @return Scope
+     */
+    public function scopeFor(Authorizable $authorizable);
 }
---
+++ src/PassthruPolicyAndScopeProduction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Deefour\Authorizer;
+
+/**
+ * Implementation of the Authorizee contract, providing a sort of reverse policy
+ * and scope resolution, starting from the authorizee instead of an authorizable.
+ */
+trait PassthruPolicyAndScopeProduction
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function policyFor(Authorizable $authorizable)
+    {
+        return $authorizable->policy($this);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scopeFor(Authorizable $authorizable)
+    {
+        return $authorizable->scope($this);
+    }
+}
