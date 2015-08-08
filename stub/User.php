e8b0c026548210d39e689764f04786f75fee0632
Jason Daly <jason@deefour.me>
2015-08-08 14:17:26 -0400

    green tests

--- stub/User.php
+++ stub/User.php
@@ -3,7 +3,9 @@
 namespace Deefour\Authorizer\Stubs;
 
 use Deefour\Authorizer\Contracts\Authorizee as AuthorizeeContract;
+use Deefour\Authorizer\PassthruPolicyAndScopeProduction;
 
 class User extends Model implements AuthorizeeContract
 {
+  use PassthruPolicyAndScopeProduction;
 }
