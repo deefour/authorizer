a7b9c29852d669d50b18d0484b6d0543eb7218c5
Jason Daly <jason@deefour.me>
2016-02-19 17:08:07 -0500

    custom resolver

--- stub/Model.php
+++ stub/Model.php
@@ -5,10 +5,19 @@ namespace Deefour\Authorizer\Stubs;
 use Deefour\Authorizer\Contracts\Authorizable;
 use Deefour\Authorizer\Contracts\Scopeable;
 use Deefour\Authorizer\ProducesPoliciesAndScopes;
+use Deefour\Producer\ProducesClasses;
 
 abstract class Model implements Authorizable, Scopeable
 {
-    use ProducesPoliciesAndScopes;
+    use ProducesPoliciesAndScopes, ProducesClasses;
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve($what)
+    {
+      return get_class($this) . ucfirst($what);
+    }
 
     /**
      * @inheritdoc
